90b2aac7f76c8dedf15aac920795efdd6afa15f8
lihongde <wy_xiaoliu@126.com>
2016-07-08 16:40:23 +0800

    初始化sql

---
+++ docs/sql/data.sql
@@ -0,0 +1,37 @@
+
+/*
+-- Query: SELECT * FROM bank.admin
+-- Date: 2016-07-08 16:31
+*/
+INSERT INTO `admin` (`id`,`login`,`password`,`role`,`login_ip`) VALUES (1,'admin','098f6bcd4621d373cade4e832627b4f6',NULL,NULL);
+INSERT INTO `admin` (`id`,`login`,`password`,`role`,`login_ip`) VALUES (2,'test','098f6bcd4621d373cade4e832627b4f6',NULL,NULL);
+
+
+/*
+-- Query: SELECT * FROM bank.system_module
+-- Date: 2016-07-08 16:28
+*/
+INSERT INTO `system_module` (`id`,`name`,`parent_module_id`,`url`) VALUES (1,'顶级节点',0,NULL);
+INSERT INTO `system_module` (`id`,`name`,`parent_module_id`,`url`) VALUES (2,'业务管理',1,'/business/manage');
+INSERT INTO `system_module` (`id`,`name`,`parent_module_id`,`url`) VALUES (3,'业绩管理',1,'/performance/manage');
+INSERT INTO `system_module` (`id`,`name`,`parent_module_id`,`url`) VALUES (4,'订单管理',1,'/order/manage');
+INSERT INTO `system_module` (`id`,`name`,`parent_module_id`,`url`) VALUES (5,'理财师管理',1,'/financial/manage');
+INSERT INTO `system_module` (`id`,`name`,`parent_module_id`,`url`) VALUES (6,'组织架构管理',1,'/organization/manage');
+INSERT INTO `system_module` (`id`,`name`,`parent_module_id`,`url`) VALUES (7,'客户管理',1,'/user/manage');
+INSERT INTO `system_module` (`id`,`name`,`parent_module_id`,`url`) VALUES (8,'产品管理',2,'/business/product');
+INSERT INTO `system_module` (`id`,`name`,`parent_module_id`,`url`) VALUES (9,'产品列表',8,'/business/product/list');
+
+/*
+-- Query: select * FROM bank.system_admin_module
+-- Date: 2016-07-08 16:32
+*/
+INSERT INTO `system_admin_module` (`id`,`admin_id`,`module_id`,`mode`) VALUES (1,1,1,'r');
+INSERT INTO `system_admin_module` (`id`,`admin_id`,`module_id`,`mode`) VALUES (2,1,2,'r');
+INSERT INTO `system_admin_module` (`id`,`admin_id`,`module_id`,`mode`) VALUES (3,1,3,'r');
+INSERT INTO `system_admin_module` (`id`,`admin_id`,`module_id`,`mode`) VALUES (4,1,4,'r');
+INSERT INTO `system_admin_module` (`id`,`admin_id`,`module_id`,`mode`) VALUES (5,1,5,'r');
+INSERT INTO `system_admin_module` (`id`,`admin_id`,`module_id`,`mode`) VALUES (6,1,6,'r');
+INSERT INTO `system_admin_module` (`id`,`admin_id`,`module_id`,`mode`) VALUES (7,1,7,'r');
+INSERT INTO `system_admin_module` (`id`,`admin_id`,`module_id`,`mode`) VALUES (8,2,2,'r');
+INSERT INTO `system_admin_module` (`id`,`admin_id`,`module_id`,`mode`) VALUES (9,2,3,'r');
+
