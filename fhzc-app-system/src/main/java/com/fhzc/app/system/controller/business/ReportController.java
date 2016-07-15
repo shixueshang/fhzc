6d659087a87e3ffd65399998b2adf2b25a253e0a
lihongde <lihongde>
2016-07-15 17:54:19 +0800

    投研报告

--- fhzc-app-system/src/main/java/com/fhzc/app/system/controller/business/ReportController.java
+++ fhzc-app-system/src/main/java/com/fhzc/app/system/controller/business/ReportController.java
@@ -1,11 +1,17 @@
 package com.fhzc.app.system.controller.business;
 
+import com.fhzc.app.system.commons.page.PageHelper;
+import com.fhzc.app.system.commons.page.PageableResult;
 import com.fhzc.app.system.controller.BaseController;
+import com.fhzc.app.system.mybatis.model.Product;
 import com.fhzc.app.system.mybatis.model.Report;
+import com.fhzc.app.system.service.ReportService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
 
 /**
  * 投研报告
@@ -15,16 +21,22 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping(value = "business/report")
 public class ReportController extends BaseController {
 
+    @Resource
+    private ReportService reportService;
+
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public ModelAndView listReport(){
         ModelAndView mav = new ModelAndView("business/report/list");
-
+        PageableResult<Report> pageableResult = reportService.findPageReports(page, size);
+        mav.addObject("page", PageHelper.getPageModel(request, pageableResult));
+        mav.addObject("reports", pageableResult.getItems());
         return mav;
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     public ModelAndView addReport(Report report){
         ModelAndView mav = new ModelAndView("business/report/list");
+        reportService.addReport(report);
         return mav;
     }
 }
--- fhzc-app-system/src/main/java/com/fhzc/app/system/service/impl/ReportServiceImpl.java
+++ fhzc-app-system/src/main/java/com/fhzc/app/system/service/impl/ReportServiceImpl.java
@@ -1,7 +1,15 @@
 package com.fhzc.app.system.service.impl;
 
+import com.fhzc.app.system.commons.page.PageableResult;
+import com.fhzc.app.system.mybatis.inter.ReportMapper;
+import com.fhzc.app.system.mybatis.model.Report;
+import com.fhzc.app.system.mybatis.model.ReportExample;
 import com.fhzc.app.system.service.ReportService;
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by lihongde on 2016/7/15.
@@ -9,4 +17,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class ReportServiceImpl implements ReportService {
 
+    @Resource
+    private ReportMapper reportMapper;
+
+    @Override
+    public PageableResult<Report> findPageReports(int page, int size) {
+        ReportExample example = new ReportExample();
+        RowBounds rowBounds = new RowBounds((page - 1) * size, size);
+        List<Report> list = reportMapper.selectByExampleWithRowbounds(example, rowBounds);
+        return new PageableResult<Report>(page, size, list.size(), list);
+    }
+
+    @Override
+    public void addReport(Report report) {
+        reportMapper.insert(report);
+    }
 }
