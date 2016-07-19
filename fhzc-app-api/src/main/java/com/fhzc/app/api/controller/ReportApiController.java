cf2bee3a1743b05025d4a1a1a3f95d34b4167939
freeman <76765357@qq.com>
2016-07-19 20:32:38 +0800

    1.增加活动、产品、投研报告list和detail接口

---
+++ fhzc-app-api/src/main/java/com/fhzc/app/api/controller/ReportApiController.java
@@ -0,0 +1,41 @@
+package com.fhzc.app.api.controller;
+
+import com.fhzc.app.api.tools.APIConstants;
+import com.fhzc.app.api.tools.ApiJsonResult;
+import com.fhzc.app.system.commons.page.PageableResult;
+import com.fhzc.app.system.mybatis.model.Product;
+import com.fhzc.app.system.mybatis.model.Report;
+import com.fhzc.app.system.service.ProductService;
+import com.fhzc.app.system.service.ReportService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by freeman on 16/7/19.
+ */
+@Controller
+public class ReportApiController {
+
+    @Resource
+    private ReportService reportService;
+
+    @RequestMapping(value = "/api/report",method = RequestMethod.GET)
+    @ResponseBody
+    public ApiJsonResult reportList(Integer userId){
+        PageableResult<Report> productList =  reportService.findPageReports(0,0);
+
+        return new ApiJsonResult(APIConstants.API_JSON_RESULT.OK,productList);
+    }
+
+    @RequestMapping(value = "/api/report/detail",method = RequestMethod.GET)
+    @ResponseBody
+    public  ApiJsonResult reporttDetail(Integer reportId){
+        Report report = reportService.getReport(reportId);
+
+        return new ApiJsonResult(APIConstants.API_JSON_RESULT.OK,report);
+    }
+}
