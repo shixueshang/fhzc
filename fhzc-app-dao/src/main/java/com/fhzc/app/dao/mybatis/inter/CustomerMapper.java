ad61561cde51c24607980c0b420c9d82600b304a
lihongde <wy_xiaoliu@126.com>
2016-07-22 17:37:49 +0800

    add Upper case file

---
+++ fhzc-app-dao/src/main/java/com/fhzc/app/dao/mybatis/inter/CustomerMapper.java
@@ -0,0 +1,33 @@
+package com.fhzc.app.dao.mybatis.inter;
+
+import com.fhzc.app.dao.mybatis.model.Customer;
+import com.fhzc.app.dao.mybatis.model.CustomerExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.session.RowBounds;
+
+public interface CustomerMapper {
+    int countByExample(CustomerExample example);
+
+    int deleteByExample(CustomerExample example);
+
+    int deleteByPrimaryKey(Integer customerId);
+
+    int insert(Customer record);
+
+    int insertSelective(Customer record);
+
+    List<Customer> selectByExampleWithRowbounds(CustomerExample example, RowBounds rowBounds);
+
+    List<Customer> selectByExample(CustomerExample example);
+
+    Customer selectByPrimaryKey(Integer customerId);
+
+    int updateByExampleSelective(@Param("record") Customer record, @Param("example") CustomerExample example);
+
+    int updateByExample(@Param("record") Customer record, @Param("example") CustomerExample example);
+
+    int updateByPrimaryKeySelective(Customer record);
+
+    int updateByPrimaryKey(Customer record);
+}
---
+++ fhzc-app-dao/src/main/java/com/fhzc/app/dao/mybatis/model/Customer.java
@@ -0,0 +1,73 @@
+package com.fhzc.app.dao.mybatis.model;
+
+public class Customer {
+    private Integer customerId;
+
+    private Integer uid;
+
+    private Integer cbId;
+
+    private Integer levelId;
+
+    private String risk;
+
+    private Integer departmentId;
+
+    private Integer bankInfoId;
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public Integer getCbId() {
+        return cbId;
+    }
+
+    public void setCbId(Integer cbId) {
+        this.cbId = cbId;
+    }
+
+    public Integer getLevelId() {
+        return levelId;
+    }
+
+    public void setLevelId(Integer levelId) {
+        this.levelId = levelId;
+    }
+
+    public String getRisk() {
+        return risk;
+    }
+
+    public void setRisk(String risk) {
+        this.risk = risk == null ? null : risk.trim();
+    }
+
+    public Integer getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Integer departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public Integer getBankInfoId() {
+        return bankInfoId;
+    }
+
+    public void setBankInfoId(Integer bankInfoId) {
+        this.bankInfoId = bankInfoId;
+    }
+}
