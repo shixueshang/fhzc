9d926b8c4e59489f7b52cb38841251c909f5f8e8
xiaoqiang.li <xiaoqiang.li@foriseassets.com>
2016-09-01 09:43:40 +0800

    remove duplicate resource

--- fhzc-app-system/src/main/java/com/fhzc/app/system/controller/business/ProductController.java
+++ fhzc-app-system/src/main/java/com/fhzc/app/system/controller/business/ProductController.java
@@ -59,8 +59,6 @@ public class ProductController extends BaseController {
     @Resource
     private AssetsService assetsService;
 
-    @Resource
-    private AssetsService assetsService;
 
     @Resource
     private PushTokenService pushTokenService;
