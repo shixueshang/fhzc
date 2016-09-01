b97b57ebaae91c16d338c2c364a217a322b5f89a
wanglong <76765357@qq.com>
2016-09-01 16:40:40 +0800

    理财师查看客户到账信息接口

--- fhzc-app-api/src/main/java/com/fhzc/app/api/service/AssetsService.java
+++ fhzc-app-api/src/main/java/com/fhzc/app/api/service/AssetsService.java
@@ -10,4 +10,5 @@ import java.util.List;
  */
 public interface AssetsService {
     List<AssetsHistory> getHistory(Integer customer_id);
+    List<AssetsHistory> getPayHistoryByPlannerId(Integer planner_id);
 }
