d5a54b69479024501ee8a0b58576e51a66e9a89e
jianwei.feng <jianwei.feng@foriseassets.com>
2016-09-09 18:03:26 +0800

    quartz定时器注释掉，改成手动触发

--- fhzc-app-system/src/main/java/com/fhzc/app/system/controller/business/CustomerDocumentController.java
+++ fhzc-app-system/src/main/java/com/fhzc/app/system/controller/business/CustomerDocumentController.java
@@ -1,7 +1,9 @@
 package com.fhzc.app.system.controller.business;
 
-import com.fhzc.app.system.controller.BaseController;
-import com.fhzc.app.system.service.CustomerDocumentService;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +12,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.annotation.Resource;
-import java.util.*;
+import com.fhzc.app.system.aop.SystemControllerLog;
+import com.fhzc.app.system.controller.BaseController;
+import com.fhzc.app.system.function.ExecuteScoreFUNC;
+import com.fhzc.app.system.service.CustomerDocumentService;
 
 /**
  * Created by Double_J on 2016/7/22
@@ -92,4 +96,52 @@ public class CustomerDocumentController extends BaseController {
         mav.addObject("url","business/customerdocument");
         return mav;
     }
+    
+    /**
+	 * 每日任务计算待审批积分
+	 *
+	 * @param multiFile
+	 * @return
+	 */
+	@RequestMapping(value = "/approvalScorePersonal", method = RequestMethod.GET)
+	@ResponseBody
+	@SystemControllerLog(description = "每日任务计算待审批积分")
+	public Map<String, Object> approvalScorePersonal() {
+		Map<String, Object> result = new HashMap<String, Object>();
+		try {
+			if(new ExecuteScoreFUNC().insertScore()){
+				result.put("result", true);
+			}else{
+				result.put("result", false);
+			}
+		} catch (Exception e) {
+			logger.error("执行失败"+ e.getMessage() );
+			result.put("result", false);
+		}
+		return result;
+	}
+	
+	/**6
+	 * 每日任务计算待审批积分
+	 *
+	 * @param multiFile
+	 * @return
+	 */
+	@RequestMapping(value = "/approvalScoreAgent", method = RequestMethod.GET)
+	@ResponseBody
+	@SystemControllerLog(description = "每日任务计算待审批积分")
+	public Map<String, Object> approvalScoreAgent() {
+		Map<String, Object> result = new HashMap<String, Object>();
+		try {
+			if(new ExecuteScoreFUNC().insertScore()){
+				result.put("result", true);
+			}else{
+				result.put("result", false);
+			}
+		} catch (Exception e) {
+			logger.error("执行失败"+ e.getMessage() );
+			result.put("result", false);
+		}
+		return result;
+	}
 }
